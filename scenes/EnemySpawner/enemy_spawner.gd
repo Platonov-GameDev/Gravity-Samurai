f289d9839934de5e443726abe856053e2984c005
Kirill Platonov <platonovk1998@ya.ru>
2024-05-28 23:59:11 +0500

    adjust spawns

--- scenes/EnemySpawner/enemy_spawner.gd
+++ scenes/EnemySpawner/enemy_spawner.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a40be67d7e1f704892588cbea0c3533a0899def6ed42e43b2e309489e7a6d5d8
-size 1278
+oid sha256:f6fc9ab7c9f2d5ba3de7a2d61480e51f107bf2fda618f202f02f51d1c0813372
+size 1662
