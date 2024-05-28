e6aae02d4cdcf04e01a0c7a2d23e6bea2ed1d5d7
Kirill Platonov <platonovk1998@ya.ru>
2024-05-28 14:24:55 +0500

    basic movement and enemy

---
+++ components/HealthComponent/health_component.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e35c527e91869eae733ab43543b814e055b5969916107fda59280a21621445f1
+size 991
---
+++ scenes/Player/player.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a42d17c27d57e2946468baa7c3ef603f49dd93f8a54d103c07db5978fd0d21e9
+size 2194
