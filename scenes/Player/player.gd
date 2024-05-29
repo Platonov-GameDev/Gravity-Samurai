abc08a01a4f13d7d1364ecdc5e5cc1d7ee080fe3
Kirill Platonov <platonovk1998@ya.ru>
2024-05-29 13:42:56 +0500

    add restart

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd6f9d1eaa158e85e21ed0a4dd64b056dde055bc4062f948dd4fb49eb7cc0844
-size 4121
+oid sha256:e7efce6e49c8390fc3ab89c670df0c8346f978c35589ef83b4cb399f2921ffae
+size 4132
