fe49643052ea1011d40ee130620eed1834591861
Kirill Platonov <platonovk1998@ya.ru>
2024-05-30 16:38:46 +0500

    polish stuff

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b94ab23b129114e3e4eea1104260bef1cce00691add3ef2189557948d0a8eb2
-size 8542
+oid sha256:85aa2060603c8ed09d6a9ba80f607eefa08a85fd3672c10133435f405b18d8f6
+size 11064
