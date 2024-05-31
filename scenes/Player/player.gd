1062cbb737e0068951543c394caf00779f4c07e7
Kirill Platonov <platonovk1998@ya.ru>
2024-05-31 15:23:51 +0500

    change gravity on walking

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88811a260d9b42e8e2e0ee493516135bf8d0ba342ae520408915c4be5802ce55
-size 12582
+oid sha256:292fbc789d10e24d05a56abda5aa0db92c5565872ee818449e2c038f1d5c3acd
+size 12804
