bedf1d33427d8f06d2b9b411ce9b4907c2276a03
Kirill Platonov <platonovk1998@ya.ru>
2024-05-31 12:40:53 +0500

    air slice

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85aa2060603c8ed09d6a9ba80f607eefa08a85fd3672c10133435f405b18d8f6
-size 11064
+oid sha256:9925916efc7b3c20a42c596a0584d93fcd60e7a59baac54641eb00828e1560d9
+size 12246
