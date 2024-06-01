1ff9843beb9f586eb0289a2cc5d2687521c18d65
Kirill Platonov <platonovk1998@ya.ru>
2024-06-01 22:15:43 +0500

    contrast screen space shader

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e772676d794d28c527c7a41aa4a1433df3b3895a20cff33854b995dba1181f26
-size 14171
+oid sha256:841a60d9a453a7631582af2f9d5eaec004604b3ab162548c7a4d217aedbc8ca5
+size 14236
