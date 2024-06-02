25893a9379ab7d5d42a2c5578747116d2f464798
Kirill Platonov <platonovk1998@ya.ru>
2024-06-02 16:17:40 +0500

    wall detector

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd90187f2bef8950c4b2b5bae1483d3babfb73012592e0863b781b7e5c002b9f
-size 16501
+oid sha256:d245623a62aa84b5dbdc48a21c5dcbb975336afab143de5fecc7467e8c071a7c
+size 17035
---
+++ scenes/WallDetector/wall_detector.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b01024ea9653e925d9981ea4d41f4016090090f217e329c38f5fddc29419f5d1
+size 1386
