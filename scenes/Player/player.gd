d7a064f30462ad7eae22f3d04f8ffeee4e3bf804
Kirill Platonov <platonovk1998@ya.ru>
2024-06-02 15:31:23 +0500

    katana rotation readjustment

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad8f25206d60fa960dff3645718b43cd83075b0601737b86897dc29c562f394e
-size 14395
+oid sha256:fd90187f2bef8950c4b2b5bae1483d3babfb73012592e0863b781b7e5c002b9f
+size 16501
